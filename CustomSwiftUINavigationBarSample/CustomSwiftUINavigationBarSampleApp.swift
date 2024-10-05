c88e060d4e2d2fcc2ad00b73e9722d0a057029de
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-10-05 11:25:35 +0300

    Add starter project

---
+++ CustomSwiftUINavigationBarSample/CustomSwiftUINavigationBarSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUINavigationBarSampleApp.swift
+//  CustomSwiftUINavigationBarSample
+//
+//  Created by Volodymyr Myroniuk on 05.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUINavigationBarSampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
